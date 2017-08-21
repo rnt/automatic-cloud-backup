e46186856aed9c95a9d7fb4f64cf41773d475809
Renato Covarrubias <rnt@rnt.cl>
2017-08-21 18:02:33 -0300

    Add support for running backup.sh inside docker container

--- backup.sh
+++ backup.sh
@@ -1,17 +1,75 @@
 #!/bin/bash
 
-CONFIG="$HOME/.backup.sh.vars"
-ATTACHMENTS="true"
-FILEPREFIX="JIRA"
+CONFIG="${CONFIG:-${HOME}/.backup.sh.vars}"
+
+USERNAME=${USERNAME:-youruser}
+PASSWORD=${PASSWORD:-yourpassword}
+INSTANCE=${INSTANCE:-example.atlassian.net}
+LOCATION=${LOCATION:-/tmp}
+
+TIMESTAMP=${TIMESTAMP:-true}
+TIMEZONE=${TIMEZONE:-America/Los_Angeles}
+SLEEP_SECONDS=${SLEEP_SECONDS:-60}
+PROGRESS_CHECKS=${PROGRESS_CHECKS:-100}
+
+ATTACHMENTS=${ATTACHMENTS:-true}
+SOURCE=${SOURCE:-jira}
+
+VERBOSE=${VERBOSE:-0}
 
 if [ -r "$CONFIG" ]; then
     . $CONFIG
-    DOWNLOAD_URL="https://${INSTANCE}"
-    INSTANCE_PATH=$INSTANCE
-else
-    echo "Usable to load $CONFIG! Please create one based on backup.sh.vars.example"
-    exit 1
 fi
+
+DOWNLOAD_URL="https://${INSTANCE}"
+INSTANCE_PATH=$INSTANCE
+
+if [[  $2 == "wiki" ]] || [[ $2 == "confluence" ]]; then
+    INSTANCE_PATH=$INSTANCE/wiki
+    DOWNLOAD_URL="https://${INSTANCE_PATH}/download"
+    FILEPREFIX="CONFLUENCE"
+else
+    FILEPREFIX="JIRA"
+fi
+
+function show_help(){
+    cat - << _EOF
+Usage:
+    $0 [--source SOURCE] [--attachments BOOL] [--timestamp BOOL] [--verbose]
+    $0 --help
+
+Arguments:
+  -a, --attachments
+    Set if attachments should be in the backup.
+    Values are "true" (default) or "false".
+    Default: "$ATTACHMENTS"
+  -s, --source
+    Set what backup should be created.
+    Set the argument to "wiki" or "confluence" to backup Confluence.
+    Default source is "jira".
+  -t, --timestamp
+    Set if we should overwrite the previous backup or append a timestamp
+    (default) to prevent just that. The former is useful when an external
+    backup program handles backup rotation.
+    Set the argument to "false" if there should be no timestamp in the
+    filename.
+  -v, --verbose
+    Enable verbose output.
+  -h, --help
+    Show this help.
+
+_EOF
+}
+
+function show_log(){
+    if [ $VERBOSE -gt 0 ]; then
+        echo "$(date "+[%Y-%m-%d %H:%M:%S]") $@"
+    fi
+}
+
+function show_error(){
+    echo "$(date "+[%Y-%m-%d %H:%M:%S]") ERROR: $@" >&2
+}
 
 while [[ $# -gt 1 ]]
 do
@@ -38,7 +96,14 @@ do
             fi
             shift # past argument
             ;;
-
+        -v|--verbose)
+            VERBOSE=$((VERBOSE+1))
+            shift # past argument
+            ;;
+        -h|--help)
+            show_help
+            exit 0
+            ;;
     esac
     shift # past argument or value
 done
@@ -47,58 +112,73 @@ BASENAME=$1
 RUNBACKUP_URL="https://${INSTANCE_PATH}/rest/obm/1.0/runbackup"
 PROGRESS_URL="https://${INSTANCE_PATH}/rest/obm/1.0/getprogress.json"
 
-# Grabs cookies and generates the backup on the UI. 
+# Grabs cookies and generates the backup on the UI.
 TODAY=$(TZ=$TIMEZONE date +%Y%m%d)
 
-#Check if we should overwrite the previous backup or append a timestamp to 
-#prevent just that. The former is useful when an external backup program handles 
-#backup rotation.
+# Check if we should overwrite the previous backup or append a timestamp to
+# prevent just that. The former is useful when an external backup program
+# handles backup rotation.
 if [ $TIMESTAMP = "true" ]; then
     OUTFILE="${LOCATION}/$FILEPREFIX-backup-${TODAY}.zip"
 elif [ $TIMESTAMP = "false" ]; then
     OUTFILE="${LOCATION}/$FILEPREFIX-backup.zip"
 else
-    echo "ERROR: invalid value for TIMESTAMP: should be either \"true\" or \"false\""
+    show_error "Invalid value for TIMESTAMP: should be either \"true\" or \"false\""
     exit 1
 fi
 
-COOKIE_FILE_LOCATION="$HOME/.backup.sh-cookie"
+COOKIE_FILE_LOCATION="$HOME/.backup.sh-$INSTANCE-cookie"
 
-# Only generate a new cookie if one does not exist, or if it is more than 24 
-# hours old. This is to allow reuse of the same cookie until a new backup can be 
-# triggered.
-find $COOKIE_FILE_LOCATION -mtime -1 2> /dev/null |grep $COOKIE_FILE_LOCATION 2>&1 > /dev/null
+# Only generate a new cookie if one does not exist, or if it is more than 24
+# hours old. This is to allow reuse of the same cookie until a new backup can
+# be triggered.
+find $COOKIE_FILE_LOCATION -mtime -1 2> /dev/null | grep $COOKIE_FILE_LOCATION 2>&1 > /dev/null
 if [ $? -ne 0 ]; then
+    show_log "Authenticate in $INSTANCE with username $USERNAME."
     curl --silent --cookie-jar $COOKIE_FILE_LOCATION -X POST "https://${INSTANCE}/rest/auth/1/session" -d "{\"username\": \"$USERNAME\", \"password\": \"$PASSWORD\"}" -H 'Content-Type: application/json' --output /dev/null
     chmod 600 $COOKIE_FILE_LOCATION
+else
+    show_log "Using previous authentication in $INSTANCE."
 fi
 
 # The $BKPMSG variable will print the error message, you can use it if you're planning on sending an email
-BKPMSG=$(curl -s --cookie $COOKIE_FILE_LOCATION --header "X-Atlassian-Token: no-check" -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json"  -X POST $RUNBACKUP_URL -d "{\"cbAttachments\":\"${ATTACHMENTS}\" }" )
+BKPMSG=$(curl -s --cookie $COOKIE_FILE_LOCATION --header "X-Atlassian-Token: no-check" -H "X-Requested-With: XMLHttpRequest" -H "Content-Type: application/json" -X POST $RUNBACKUP_URL -d "{\"cbAttachments\":\"${ATTACHMENTS}\" }" )
 
 # Checks if we were authorized to create a new backup
 if [ "$(echo "$BKPMSG" | grep -c Unauthorized)" -ne 0 ]  || [ "$(echo "$BKPMSG" | grep -ic "<status-code>401</status-code>")" -ne 0 ]; then
-    echo "ERROR: authorization failure"
-    exit
+    show_error "Authorization failure in $INSTANCE with username $USERNAME"
+    exit 2
 fi
+
+set -x
 
 #Checks if the backup exists every 10 seconds, 20 times. If you have a bigger instance with a larger backup file you'll probably want to increase that.
 for (( c=1; c<=$PROGRESS_CHECKS; c++ )) do
     PROGRESS_JSON=$(curl -s --cookie $COOKIE_FILE_LOCATION $PROGRESS_URL)
-    FILE_NAME=$(echo "$PROGRESS_JSON" | sed -n 's/.*"fileName"[ ]*:[ ]*"\([^"]*\).*/\1/p')
+    FILE_NAME=$(echo "$PROGRESS_JSON" | jq --raw-output ".fileName")
+    FILE_SIZE=$(echo "$PROGRESS_JSON" | jq --raw-output ".size")
+    currentStatus=$(echo "$PROGRESS_JSON" | jq --raw-output ".currentStatus")
+    alternativePercentage=$(echo "$PROGRESS_JSON" | jq --raw-output ".alternativePercentage")
 
-    echo $PROGRESS_JSON|grep error > /dev/null && break
+    echo $PROGRESS_JSON | grep error > /dev/null && break
 
-    if [ ! -z "$FILE_NAME" ]; then
+    if [ -n "$FILE_NAME" ] && [ "$FILE_NAME" != "null" ]; then
         break
     fi
-    sleep $SLEEP_SECONDS
+    if [ $c -lt $PROGRESS_CHECKS ]; then
+        show_log "($c of $PROGRESS_CHECKS) $currentStatus. $alternativePercentage"
+        sleep $SLEEP_SECONDS
+    fi
 done
 
 # If after 20 attempts it still fails it ends the script.
-if [ -z "$FILE_NAME" ]; then
-    exit
+if [ -z "$FILE_NAME" ] || [ "$FILE_NAME" == "null" ]; then
+    show_error "After $PROGRESS_CHECKS checks, the backup isn't ready."
+    exit 3
 else
     # Download the new way, starting Nov 2016
+    show_log "Downloading backup $FILE_SIZE Bytes from $FILE_NAME to $OUTFILE"
     curl -s -S -L --cookie $COOKIE_FILE_LOCATION "$DOWNLOAD_URL/$FILE_NAME" -o "$OUTFILE"
 fi
+
+show_log "All done ;)"
